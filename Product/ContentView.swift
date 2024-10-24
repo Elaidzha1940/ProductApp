c08e395a56e97acc227b99ba57f331e259f7d206
Elaidzha1940 <eli080293@gmail.com>
2024-10-24 18:43:22 +0300

    ProductApp

---
+++ Product/ContentView.swift
@@ -0,0 +1,23 @@
+//  /*
+//
+//  Project: Product
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 24.10.2024
+//
+//  */ 
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+           
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Product/ProductApp.swift
@@ -0,0 +1,19 @@
+//  /*
+//
+//  Project: Product
+//  File: ProductApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 24.10.2024
+//
+//  */
+
+import SwiftUI
+
+@main
+struct ProductApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
